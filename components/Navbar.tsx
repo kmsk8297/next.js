92b253a19ff029cb5796e080f7ab7ed442c44d58
putturudivakar <putturudivakarreddy@gmail.com>
2024-09-25 15:27:02 +0530

    first commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,10 +17,10 @@ const Navbar = () => {
           /> */}
         </div>
         <h1 className="text-white text-[25px] font-semibold">
-          Webchain{" "}
+          WebPage{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
             {" "}
-            Dev{" "}
+            Art{" "}
           </span>
         </h1>
       </div>
